5aa7bae379b5d4f7f204f6c84be822850e52ed5b
melika-nasehi <130899479+melika-nasehi@users.noreply.github.com>
2026-02-27 14:47:43 +0330

    create views for Plans

--- my_finance/plans/views.py
+++ my_finance/plans/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Asset, CashFlow, PlanEvent
+from .serializers import AssetSerializer, CashFlowSerializer, PlanEventSerializer
 
 # Create your views here.
+
+class AssetViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = AssetSerializer
+
+    def get_queryset(self):
+        return Asset.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CashFlowViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CashFlowSerializer
+
+    def get_queryset(self):
+        return CashFlow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PlanEventViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PlanEventSerializer
+
+    def get_queryset(self):
+        return PlanEvent.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
